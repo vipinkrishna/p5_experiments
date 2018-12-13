9aa0b69d72c41f4f411622b819131688dcab30cf
vipinkrishna <vipinkrishna.in@gmail.com>
2018-12-13 12:28:32 +0530

    ellipse

---
+++ 001-ellipse/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+}
+
+function draw() {
+  // put drawing code here
+  background(220);
+  ellipse(50,50,80,80);
+  ellipse(200,200,200,240);
+}
